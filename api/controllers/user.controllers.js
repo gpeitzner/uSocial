3c8cafc710364448efec187604969e8eca4905a0
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-10-23 00:28:56 -0600

    feat: find one user

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -3,10 +3,19 @@ const userSchema = require('../models/user');
 const user = {};
 
 //get user 
-user.getusers = async (req, res) => {    
+user.getusers = async (req, res) => {
     const user = await userSchema.find();
     res.json(user);
 };
+
+user.getOneUser = async (req, res) =>{
+    try{
+        const user = await userSchema.findById(req.params._id);
+        res.json(user)
+    }catch (e){
+        res.status(403).send("Usuario no existe.")
+    }
+}
 
 //create user
 user.createUser = async (req, res) => {
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,5 +3,6 @@ const userControllers = require('../controllers/user.controllers');
 const router = express.Router();
 
 router.get('/', userControllers.getusers);
-router.post('/addUser', userControllers.createUser);
+router.post('/user/registrer', userControllers.createUser);
+router.get('/user/:_id', userControllers.getOneUser);
 module.exports = router;
