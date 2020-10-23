3cc7608d5fb168026b04c10c79e01edcac5bcc48
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-10-23 17:10:25 -0600

    [add] lambda countrys filter applied

--- aws/lambda.js
+++ aws/lambda.js
@@ -22,17 +22,31 @@ function getData() {
 }
 
 exports.handler = async (event) => {
+  const { country, start, end } = event;
   try {
     const results = await getData();
-    const response = {
-      statusCode: 200,
-      body: results,
-    };
-    return response;
+    const countryData = results[country];
+    if (country && start && end) {
+      const daysInfo = countryData.filter(
+        (day) => day.date >= start && day.date <= end
+      );
+      const response = {
+        statusCode: 200,
+        body: daysInfo,
+      };
+      return response;
+    } else {
+      const dayInfo = countryData.filter((day) => day.date === start);
+      const response = {
+        statusCode: 200,
+        body: dayInfo,
+      };
+      return response;
+    }
   } catch (error) {
     const response = {
       statusCode: 500,
-      body: error,
+      body: JSON.stringify(error),
     };
     return response;
   }
