db6f886238837c1ab0e861128869826ed70c22f3
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-10-22 23:20:17 -0600

    config: limit in urlencoded and json of express

--- api/index.js
+++ api/index.js
@@ -10,7 +10,8 @@ app.set('port', process.env.PORT || 3000);
 
 //midleware
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cors()); // direccion del servidor de angular 
 
 
