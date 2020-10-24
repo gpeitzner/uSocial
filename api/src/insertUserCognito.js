b808c8dbb7c29970c26fbd24597822a3ab05fa00
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-10-23 19:26:20 -0600

    fix: boolean modebot

--- api/src/insertUserCognito.js
+++ api/src/insertUserCognito.js
@@ -25,7 +25,7 @@ module.exports = (req) => {
             },
             {
                 Name: "custom:modeBot",
-                Value: false
+                Value: false + ''
             }
         ]
     };
